5b6822453c0317dc7df1dadd53522d1c6f602400
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2020-02-16 23:06:20 -0500

    Moved some of the conditionals to private methods

--- src/main/java/com/oblivion/BowlingGame.java
+++ src/main/java/com/oblivion/BowlingGame.java
@@ -14,19 +14,34 @@ public class BowlingGame {
         int frameIndex = 0;
         int frames;
         for (frames = 0; frames < 10; frames++) {
-            if(rolls[frameIndex]==10){
-                score+=10+rolls[frameIndex + 1]+rolls[frameIndex + 2];
-                frameIndex+=1;
-            }
-            else if (rolls[frameIndex] + rolls[frameIndex + 1] == 10) {
-                score += 10 + rolls[frameIndex + 2];
+            if (rolls[frameIndex] == 10) {
+                score += 10 + strikeBonus(frameIndex);
+                frameIndex += 1;
+            } else if (isSpare(frameIndex)) {
+                score += 10 + spareBonus(frameIndex);
                 frameIndex += 2;
             } else {
-                score += rolls[frameIndex] + rolls[frameIndex + 1];
+                score += sumOfBallsInFrame(frameIndex);
                 frameIndex += 2;
             }
 
         }
         return score;
+    }
+
+    private int sumOfBallsInFrame(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1];
+    }
+
+    private boolean isSpare(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+    }
+
+    private int spareBonus(int frameIndex) {
+        return rolls[frameIndex + 2];
+    }
+
+    private int strikeBonus(int frameIndex) {
+        return rolls[frameIndex + 1] + rolls[frameIndex + 2];
     }
 }
