b79f7a288211471489a40d1f302d2cb6c91f5b28
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2020-02-16 21:17:05 -0500

    Moved the Game creation step to Setup

--- src/test/java/com/oblivion/BowlingGameTest.java
+++ src/test/java/com/oblivion/BowlingGameTest.java
@@ -1,5 +1,6 @@
 package com.oblivion;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -8,32 +9,33 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class BowlingGameTest {
 
+    private BowlingGame game;
 
-    @Test
-    public void scoreShouldBeZeroAtStart(){
-        BowlingGame game = new BowlingGame();
-        game.roll(0);
-        assertThat(game.score(),is(equalTo(0)));
+    @Before
+    public void setup() {
+        game = new BowlingGame();
     }
 
     @Test
-    public void scoreZeroForAllZeroes(){
-        BowlingGame game = new BowlingGame();
-        for(int i=0;i<20;i++) {
+    public void scoreShouldBeZeroAtStart() {
+        game.roll(0);
+        assertThat(game.score(), is(equalTo(0)));
+    }
+
+    @Test
+    public void scoreZeroForAllZeroes() {
+        for (int i = 0; i < 20; i++) {
             game.roll(0);
         }
-        assertThat(game.score(),is(equalTo(0)));
+        assertThat(game.score(), is(equalTo(0)));
     }
 
     @Test
-    public void scoreForAllOnes(){
-        BowlingGame game = new BowlingGame();
-        for(int i=0;i<20;i++) {
+    public void scoreForAllOnes() {
+        for (int i = 0; i < 20; i++) {
             game.roll(1);
         }
-        assertThat(game.score(),is(equalTo(20)));
+        assertThat(game.score(), is(equalTo(20)));
     }
-
-
 
 }
