6c4bebb924bb5860dba3b0e6d90408cce0468d96
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2020-02-16 21:14:40 -0500

    Updated code to handle scores in roll method

--- src/main/java/com/oblivion/BowlingGame.java
+++ src/main/java/com/oblivion/BowlingGame.java
@@ -1,11 +1,12 @@
 package com.oblivion;
 
 public class BowlingGame {
-
-    public void roll(int i) {
+    int score=0;
+    public void roll(int pins) {
+        score+=pins;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
